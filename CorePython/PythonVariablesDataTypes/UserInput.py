904a18e46e10cf4c556c7a66fbe35a74dd6019b0
Shankar Prasad Dahal <49245870+pdsdahal@users.noreply.github.com>
2024-05-18 20:19:38 -0500

    User input in Python
    User input in Python

---
+++ CorePython/PythonVariablesDataTypes/UserInput.py
@@ -0,0 +1,7 @@
+#input() function always returns user input as a string
+
+studentName = input("Enter Student Name")
+age = int(input("Enter Student Age"))
+familyIncome = float(input("Enter you family monthly income"))
+
+print(f" Name : {studentName}\n Age : {age}\n Family Monthly Income : {familyIncome}")
