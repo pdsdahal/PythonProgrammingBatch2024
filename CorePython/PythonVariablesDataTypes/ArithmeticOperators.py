05b71d9ddd7d01f129a1655ab10f3d92c9fe3073
Shankar Prasad Dahal <49245870+pdsdahal@users.noreply.github.com>
2024-05-22 16:47:58 -0500

    Arithmetic operators and Assignment operators
    Arithmetic operators and Assignment operators

---
+++ CorePython/PythonVariablesDataTypes/ArithmeticOperators.py
@@ -0,0 +1,18 @@
+num1 = int(input("Enter a first integer number"))
+num2 = int(input("Enter a second integer number"))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+modulus = num1 % num2
+floorDivision = num1 // num2
+exponent = num1 ** num2
+
+print(f"{num1} + {num2}  = {addition}")
+print(f"{num1} - {num2}  = {subtraction}")
+print(f"{num1} * {num2}  = {multiplication}")
+print(f"{num1} / {num2}  = {division}")
+print(f"{num1} % {num2}  = {modulus}")
+print(f"{num1} // {num2}  = {floorDivision}")
+print(f"{num1} ** {num2}  = {exponent}")
---
+++ CorePython/PythonVariablesDataTypes/AssignmentOperators.py
@@ -0,0 +1,16 @@
+num = 10
+
+num += 5 # num = num + 5 = 10 + 5 = 15
+print(f"num += 5 : {num}")
+
+num -= 10 # num = num - 10 = 15 - 10 = 5
+print(f"num -= 5 : {num}")
+
+num *= 2 # num = num * 2 = 5 * 2 = 10
+print(f"num *= 5 : {num}")
+
+num /= 5 # num = num /5 = 10 / 5 = 2
+print(f"num /= 5 : {num}")
+
+num %= 2 # num = num % 2 = 2%2 = 0
+print(f"num %= 2 : {num}")
